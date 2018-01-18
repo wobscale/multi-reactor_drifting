76b46c2861d17deae588d3b4b1b8994bc9fabfa9
Euan Kemp <euank@euank.com>
2018-01-17 23:33:51 -0800

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,57 @@
+#![cfg_attr(test, feature(plugin, custom_derive))]
+#![cfg_attr(test, plugin(rocket_codegen))]
+
+#[cfg(test)]
+mod tests;
+
+extern crate futures;
 extern crate rocket;
 extern crate tokio_core;
-extern crate futures;
 
 use tokio_core::reactor::Core;
 use rocket::request::Outcome as ReqOutcome;
 use rocket::Outcome;
 use rocket::request::Request;
-use rocket::response::{Response, Responder};
+use rocket::response::{Responder, Response};
 use rocket::http::Status;
 use std::sync::Mutex;
 
+///
+/// The Handle struct may be used to get access to a tokio core handle for this request.
+/// A future for this handle may be returned from the request handler, at which point it will be
+/// resolved.
+/// A future derived from this handle may also be run using the `request_core_run` method.
+///
+/// Handle implements 'into' for a tokio_core Handle to allow for easy conversion.
 pub struct Handle {
     handle: tokio_core::reactor::Handle,
 }
 
+// Cores cannot be sent between threads safely, so we instantiate one per thread.
+// Since 'running' a reactor core requires mutable ownership, we wrap it in a mutex.
 thread_local! {
     static CORE: Mutex<Core> = Mutex::new(Core::new().unwrap());
 }
 
-pub fn request_core_run<F>(f: F) -> Result<F::Item, F::Error> 
-    where F: futures::future::Future,
+///
+/// Run a future to completion. This will block the handling of the current request until the
+/// future resolves.
+pub fn request_core_run<F>(f: F) -> Result<F::Item, F::Error>
+where
+    F: futures::future::Future,
 {
-    CORE.with(|c| {
-        c.lock().unwrap().run(f)
-    })
+    CORE.with(|c| c.lock().unwrap().run(f))
 }
 
 impl<'a, 'r> rocket::request::FromRequest<'a, 'r> for Handle {
     type Error = ();
 
     fn from_request(_: &rocket::Request) -> ReqOutcome<Self, Self::Error> {
-        let handle = CORE.with(|f| {
-            f.lock().unwrap().handle()
-        });
+        let handle = CORE.with(|f| f.lock().unwrap().handle());
 
-        Outcome::Success(Handle{handle: handle})
+        Outcome::Success(Handle { handle: handle })
     }
 }
-
 
 impl From<Handle> for tokio_core::reactor::Handle {
     fn from(h: Handle) -> tokio_core::reactor::Handle {
@@ -45,7 +59,10 @@ impl From<Handle> for tokio_core::reactor::Handle {
     }
 }
 
+/// Future is a thin wrapper for a futures future. This type implements the Responder trait,
+/// which allows it to be returned from a rocket request handler.
 pub struct Future<I, E>(pub Box<futures::future::Future<Item = I, Error = E>>);
+// TODO: there must be a better way than boxing it up
 
 impl<I, E> futures::future::Future for Future<I, E> {
     type Item = I;
@@ -57,7 +74,8 @@ impl<I, E> futures::future::Future for Future<I, E> {
 }
 
 impl<'r, I, E> rocket::response::Responder<'r> for Future<I, E>
-    where Result<I, E>: Responder<'r>,
+where
+    Result<I, E>: Responder<'r>,
 {
     fn respond_to(self, request: &Request) -> Result<Response<'r>, Status> {
         CORE.with(|core_m| {
---
+++ src/tests.rs
@@ -0,0 +1,36 @@
+use rocket;
+use super::*;
+use rocket::local::Client;
+use futures;
+
+#[get("/")]
+fn future_string() -> Future<String, ()> {
+    Future(Box::new(futures::future::ok("hello world".to_owned())))
+}
+
+#[test]
+fn it_works() {
+    let rkt = rocket::ignite().mount("/", routes![future_string]);
+    let c = Client::new(rkt).unwrap();
+
+    let mut resp = c.get("/").dispatch();
+    assert_eq!(resp.status(), Status::Ok);
+    assert_eq!(resp.body_string(), Some("hello world".to_owned()));
+}
+
+#[get("/")]
+fn mid_request_future() -> String {
+    let s = future_string();
+
+    request_core_run(s).unwrap()
+}
+
+#[test]
+fn request_core_run_works() {
+    let rkt = rocket::ignite().mount("/", routes![mid_request_future]);
+    let c = Client::new(rkt).unwrap();
+
+    let mut resp = c.get("/").dispatch();
+    assert_eq!(resp.status(), Status::Ok);
+    assert_eq!(resp.body_string(), Some("hello world".to_owned()));
+}
